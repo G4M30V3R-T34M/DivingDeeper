ea01385511e9b75c81256c0f0406d8b66dbae390
Jaume Riera <jaumerg95@gmail.com>
2023-08-22 22:39:41 +0200

    Remove Interactable interface

--- Assets/Scripts/Test_scripts/TestInteractableItem.cs
+++ Assets/Scripts/Test_scripts/TestInteractableItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestInteractableItem : MonoBehaviour, IInteractable
+public class TestInteractableItem : MonoBehaviour
 {
     [SerializeField]
     GameObject interactableFeedback;
