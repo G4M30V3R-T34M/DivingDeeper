76018a2e964f2eea3f0132d2c0262df8918c6265
Jaume Riera <jaumerg95@gmail.com>
2023-08-22 22:36:28 +0200

    Remove check for player layer and set collider as requirement

--- Assets/Scripts/EventListeners/OnCollisionGameEventListener.cs
+++ Assets/Scripts/EventListeners/OnCollisionGameEventListener.cs
@@ -2,19 +2,16 @@ using UnityEngine;
 using UnityEngine.Events;
 using FeTo.SOArchitecture;
 
+[RequireComponent(typeof(Collider2D))]
 public class OnCollisionGameEventListener : GameEventListener
 {
 
     protected void OnTriggerEnter2D(Collider2D col) {
-        if(col.gameObject.layer == (int)Layer.Player) {
-            Event.RegisterListener(this);
-        }
+        Event.RegisterListener(this);
     }
 
     protected void OnTriggerExit2D(Collider2D col) {
-        if (col.gameObject.layer == (int)Layer.Player) {
-            Event.UnregisterListener(this);
-        }
+        Event.UnregisterListener(this);
     }
 
     protected new void OnEnable() { }
