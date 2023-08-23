22965b0a8bfe577d225d68bf50fe2813a447ac5d
Jaume Riera <jaumerg95@gmail.com>
2023-08-23 15:22:26 +0200

    Allow Interations triggered on hold

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,15 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private GameEvent playerInteractEvent;
+    [SerializeField]
+    private GameEvent playerHoldEvent;
+    [SerializeField]
+    private GameEvent playerHoldReleaseEvent;
+
+    private bool isHolding = false;
 
     public void Interact(InputAction.CallbackContext context) {
-        if(context.performed) {
-            playerInteractEvent.Raise();
+        if(context.interaction is PressInteraction) {
+            if (context.performed) {
+                playerInteractEvent.Raise();
+            }
+            return;
+        }
+        if(context.interaction is HoldInteraction) {
+            if (context.performed) {
+                isHolding = true;
+                playerHoldEvent.Raise();
+            }
+            if (context.canceled && isHolding) {
+                isHolding = false;
+                playerHoldReleaseEvent.Raise();
+            }
         }
     }
 }
