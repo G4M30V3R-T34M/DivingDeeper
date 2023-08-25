267ddceb278690d870a4a4500687a9a7af2353a5
Neengash <pepmanas93@gmail.com>
2023-08-25 21:49:56 +0200

    Add QuestBehaviour script

---
+++ Assets/Scripts/Quest/QuestBehaviour.cs
@@ -0,0 +1,49 @@
+using FeTo.ServiceLocator;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class QuestBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    QuestScriptableObject quest;
+
+    [SerializeField]
+    UnityEvent OnBlocked;
+
+    [SerializeField]
+    UnityEvent OnAvailable;
+
+    [SerializeField]
+    UnityEvent OnCompleted;
+
+    IQuestManager questManager;
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+        questManager = ServiceLocator.Instance.GetService<IQuestManager>();
+        if (questManager.IsBloqued(quest.Id))
+        {
+            OnBlocked?.Invoke();
+        } else if (questManager.IsAvailable(quest.Id))
+        {
+            OnAvailable?.Invoke();
+        } else if (questManager.IsCompleted(quest.Id))
+        {
+            OnCompleted?.Invoke();
+        }
+    }
+
+    public void QuestAvailable(string questId)
+    {
+        if (questId == quest.Id) { OnAvailable?.Invoke(); }
+    }
+
+    public void QuestCompleted(string questId)
+    {
+        if (questId == quest.Id) { OnCompleted?.Invoke(); }
+    }
+}
