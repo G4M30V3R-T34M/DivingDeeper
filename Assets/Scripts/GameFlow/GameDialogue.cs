fd4a1801929d0cc521cb2858b49fdce2a8fd79af
Neengash <pepmanas93@gmail.com>
2023-08-26 07:51:53 +0200

    Fixes on sceneTransition

---
+++ Assets/Scripts/GameFlow/GameDialogue.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameDialogue : MonoBehaviour
+{
+    [SerializeField]
+    ConversationScriptableObject conversation;
+
+    [SerializeField]
+    QuestScriptableObject quest;
+
+    public void StartDialogue()
+    {
+        ConversationPlayer.Instance.Play(conversation, quest);
+    }
+
+}
